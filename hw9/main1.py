949997e16bb63bc2246bc2518221e0da723e1e8d
ArtemIsmagilov <118372045+ArtemIsmagilov@users.noreply.github.com>
2023-02-24 11:49:52 +0400

    change `<` on `<=` in main1.py

--- hw9/main1.py
+++ hw9/main1.py
@@ -4,9 +4,9 @@ import numpy as np
 #Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population)
 df = pd.read_csv('sample_data/california_housing_train.csv')
 dataset_1 = df[['population', 'median_income']]
-qr = dataset_1.query('0 < population < 500 ')
+qr = dataset_1.query('0 <= population <= 500 ')
 res = qr[['median_income']].mean().item()
-print(res) #3.87454440974391
+print(res) #3.8777114641744546
 
 #Узнать какая максимальная households в зоне минимального значения population
 dataset_2 = df[['population' ,'households']]
